b4149ad6b338b84791c2647afd93f6c48f24dbc9
sharonunnikrishnan06 <sharonunnikrishnan06@gmail.com>
2026-05-29 07:25:43 +0000

    Added expense module

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExpenseController extends Controller
+{
+    public function index()
+    {
+        return "Expense Module Created from Ubuntu VM";
+    }
+}
